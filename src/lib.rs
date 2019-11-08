4f9887e98a0a5704dec2b1f153a687c797ab0ea5
Piet Geursen <pietgeursen@gmail.com>
2019-11-08 12:55:36 +0100

    Removes lots of `unwrap` calls. Propogate `Error`s up to caller.

--- src/lib.rs
+++ src/lib.rs
@@ -1,142 +1,210 @@
+use arrayvec::ArrayVec;
 use base64::decode_config_slice;
 use ed25519_dalek::{verify_batch as dalek_verify_batch, PublicKey, Signature};
 use rayon::prelude::*;
 use regex::bytes::Regex;
+use serde::{Deserialize, Serialize};
+use serde_json;
+use serde_json::Error as SerdeJsonError;
 use sha2::Sha512;
-use ssb_legacy_msg_data::json::{from_slice, to_string};
+use snafu::{OptionExt, ResultExt, Snafu};
+use ssb_legacy_msg_data::json::{from_slice, to_string, DecodeJsonError, EncodeJsonError};
 use ssb_legacy_msg_data::value::Value;
-use std::borrow::Cow;
 
 #[macro_use]
 extern crate lazy_static;
 
+#[derive(Debug, Snafu)]
+pub enum Error {
+    #[snafu(display("Signature string could not be parsed into a valid ed25519 sig"))]
+    InvalidSignatureBytes,
+    #[snafu(display("Key string could not be parsed into a valid ed25519 sig"))]
+    InvalidKeyBytes,
+    #[snafu(display("Signature was invalid"))]
+    InvalidSignature {},
+    #[snafu(display("Error parsing ssb message, it is invalid. Errored with: {}", source))]
+    InvalidSsbMessage { source: DecodeJsonError },
+    #[snafu(display("Error encoding ssb message, it is invalid. Errored with: {}", source))]
+    UnableToEncodeMessageToValidSigningEncoding { source: EncodeJsonError },
+    #[snafu(display(
+        "Error parsing ssb message as json, it is invalid. Errored with: {}",
+        source
+    ))]
+    InvalidSsbMessageJson { source: SerdeJsonError },
+    #[snafu(display("The signature was not a valid ssb ed25519 signature string."))]
+    InvalidSignatureString {},
+    #[snafu(display("The signature was not valid base64 encoding. {}", source))]
+    InvalidSignatureStringBase64Encoding { source: base64::DecodeError },
+    #[snafu(display("The author pub key was not a valid ssb ed25519 pub key string."))]
+    InvalidAuthorString {},
+    #[snafu(display("The author pub key was not valid base64 encoding. {}", source))]
+    InvalidAuthorStringBase64Encoding { source: base64::DecodeError },
+    #[snafu(display("Unable to get the value from the message, the message was invalid"))]
+    InvalidMessageNoValue,
+}
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+type KeySigBytes = (PublicKey, Signature, Vec<u8>);
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SsbMessageValue<'a> {
+    signature: &'a str,
+    author: &'a str,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SsbMessage<'a> {
+    #[serde(borrow)]
+    value: SsbMessageValue<'a>,
+}
+
 lazy_static! {
-    static ref SIGNATURE_REGEX: Regex =
-        Regex::new(r##"(,\n\s+"signature":\s".+.ed25519")"##).unwrap();
     static ref SIGNATURE_BYTES_REGEX: Regex =
-        Regex::new(r##"\n\s+"signature":\s"(.+).sig.ed25519""##).unwrap();
+        Regex::new(r##"([A-Za-z0-9\\/+]{86}==).sig.ed25519"##).unwrap();
     static ref PUBKEY_BYTES_REGEX: Regex =
-        Regex::new(r##"\n\s+"author":\s"@(.+).ed25519""##).unwrap();
+        Regex::new(r##"@([A-Za-z0-9\\/+]{43}=).ed25519"##).unwrap();
 }
 
-pub fn verify(msg: &[u8]) -> bool {
-    let (key, sig, bytes) = get_pubkey_sig_bytes_from_ssb_message(msg);
-    key.verify::<Sha512>(&bytes, &sig).is_ok()
+pub fn verify(msg: &[u8]) -> Result<()> {
+    let (key, sig, bytes) = get_pubkey_sig_bytes_from_ssb_message(msg)?;
+    key.verify::<Sha512>(&bytes, &sig)
+        .map_err(|_| snafu::NoneError)
+        .context(InvalidSignature)
 }
 
-pub fn par_verify(msgs: &[&[u8]]) -> bool {
-    msgs.par_iter().all(|msg| verify(msg))
-}
-
-pub fn par_verify_batch(msgs: &[&[u8]]) -> bool {
+pub fn par_verify(msgs: &[&[u8]]) -> Result<()> {
+    // Ok, I know this looks weird. But we want to be able to try and verify all msgs but abort
+    // with Error when we hit something that did not verify.
     msgs.par_iter()
-        .map(|msg| get_pubkey_sig_bytes_from_ssb_message(msg))
-        .chunks(200)
-        .all(|chunk| {
-            //each chunk is a collection of (key, sig, bytes)
-            let keys = chunk.iter().map(|(key, _, _)| *key).collect::<Vec<_>>();
-            let sigs = chunk.iter().map(|(_, sig, _)| *sig).collect::<Vec<_>>();
-            let bytes = chunk
-                .iter()
-                .map(|(_, _, msg)| msg.as_slice())
-                .collect::<Vec<_>>();
-
-            dalek_verify_batch::<Sha512>(bytes.as_slice(), sigs.as_slice(), keys.as_slice()).is_ok()
-        })
+        .try_fold(|| (), |_, msg| verify(msg))
+        .try_reduce(|| (), |_, _| Ok(()))
 }
 
-fn get_pubkey_sig_bytes_from_ssb_message<'a>(msg: &'a [u8]) -> (PublicKey, Signature, Vec<u8>) {
+const CHUNK_SIZE: usize = 50;
+
+pub fn par_verify_batch(msgs: &[&[u8]]) -> Result<()> {
+    msgs.par_iter()
+        .chunks(CHUNK_SIZE)
+        .try_fold(
+            || (),
+            |_, chunk| {
+                let keys_sigs_bytes = chunk.iter()
+                    .flat_map(|msg| get_pubkey_sig_bytes_from_ssb_message(msg))
+                    .collect::<ArrayVec<[KeySigBytes; CHUNK_SIZE]>>();
+                //each chunk is a collection of (key, sig, bytes)
+                let keys = keys_sigs_bytes
+                    .iter()
+                    .map(|(key, _, _)| *key)
+                    .collect::<ArrayVec<[_; CHUNK_SIZE]>>();
+                let sigs = keys_sigs_bytes
+                    .iter()
+                    .map(|(_, sig, _)| *sig)
+                    .collect::<ArrayVec<[_; CHUNK_SIZE]>>();
+                let bytes = keys_sigs_bytes
+                    .iter()
+                    .map(|(_, _, msg)| msg.as_slice())
+                    .collect::<ArrayVec<[_; CHUNK_SIZE]>>();
+
+                dalek_verify_batch::<Sha512>(bytes.as_slice(), sigs.as_slice(), keys.as_slice())
+                    .map_err(|_| snafu::NoneError)
+                    .context(InvalidSignature)
+            },
+        )
+        .try_reduce(|| (), |_, _| Ok(()))
+}
+
+fn get_pubkey_sig_bytes_from_ssb_message<'a>(
+    msg: &'a [u8],
+) -> Result<KeySigBytes> {
+    let mut verifiable_msg: Value = from_slice(&msg).context(InvalidSsbMessage)?;
+    let message: SsbMessage = serde_json::from_slice(msg).context(InvalidSsbMessageJson)?;
+
     // Parse the signature string
-    let sig_bytes = get_sig_bytes(msg);
+    let sig_bytes = get_sig_bytes(message.value.signature.as_bytes())?;
     // Parse the author string
-    let key_bytes = get_key_bytes(msg);
+    let key_bytes = get_key_bytes(message.value.author.as_bytes())?;
 
     // Convert the signature bytes to a dalek signature
-    let key = PublicKey::from_bytes(&key_bytes).unwrap();
+    let key = PublicKey::from_bytes(&key_bytes)
+        .map_err(|_| snafu::NoneError)
+        .context(InvalidKeyBytes)?;
 
     // Convert the author bytes to a dalek pub key
-    let sig = Signature::from_bytes(&sig_bytes).unwrap();
+    let sig = Signature::from_bytes(&sig_bytes)
+        .map_err(|_| snafu::NoneError)
+        .context(InvalidSignatureBytes)?;
 
-    // We need the string of the value as that's what's signed (without the sig)
-    let value = extract_value_from_entry(msg);
-    // Remove the signature
-    let bytes_to_verify = remove_signature_from_entry(value.as_bytes());
+    // Modify the val by removing the signature
+    if let Value::Object(ref mut msg) = verifiable_msg {
+        if let Some(Value::Object(ref mut v)) = msg.get_mut("value") {
+            v.remove("signature".to_owned());
+        }
+    };
 
-    (key, sig, bytes_to_verify.to_vec())
-}
-
-fn get_sig_bytes<'a>(msg: &'a [u8]) -> [u8; 64] {
-    let caps = SIGNATURE_BYTES_REGEX.captures(msg).unwrap();
-    let sig_str = caps.get(1).unwrap();
-    let mut buff = [0; 64];
-    decode_config_slice(sig_str.as_bytes(), base64::STANDARD, &mut buff).unwrap();
-    buff
-}
-
-fn get_key_bytes<'a>(msg: &'a [u8]) -> [u8; 32] {
-    let caps = PUBKEY_BYTES_REGEX.captures(msg).unwrap();
-    let key_str = caps.get(1).unwrap();
-    let mut buff = [0; 32];
-    decode_config_slice(key_str.as_bytes(), base64::STANDARD, &mut buff).unwrap();
-    buff
-}
-
-fn remove_signature_from_entry<'a>(msg: &'a [u8]) -> Cow<'a, [u8]> {
-    SIGNATURE_REGEX.replace_all(msg, &b""[..])
-}
-
-fn extract_value_from_entry<'a>(msg: &'a [u8]) -> String {
-    let val: Value = from_slice(&msg).unwrap();
-
-    let value = match val {
-        Value::Object(ref o) => o.get("value").unwrap(),
+    // Get the value from the message as this is what was signed
+    let verifiable_msg_value = match verifiable_msg {
+        Value::Object(ref mut o) => o.get("value").context(InvalidMessageNoValue)?,
         _ => panic!(),
     };
-    to_string(value, false).unwrap()
+
+    // Encode the message to a string with out the signature. This was how it was when it was
+    // signed by the author.
+    let bytes_to_verify = to_string(&verifiable_msg_value, false)
+        .context(UnableToEncodeMessageToValidSigningEncoding)?;
+
+    Ok((key, sig, bytes_to_verify.into_bytes()))
+}
+
+fn get_sig_bytes<'a>(sig: &'a [u8]) -> Result<[u8; 64]> {
+    let caps = SIGNATURE_BYTES_REGEX
+        .captures(sig)
+        .context(InvalidSignatureString)?;
+    let sig_str = caps.get(1).context(InvalidSignatureString)?;
+    let mut buff = [0; 64];
+    decode_config_slice(sig_str.as_bytes(), base64::STANDARD, &mut buff)
+        .context(InvalidSignatureStringBase64Encoding)?;
+    Ok(buff)
+}
+
+fn get_key_bytes<'a>(key: &'a [u8]) -> Result<[u8; 32]> {
+    let caps = PUBKEY_BYTES_REGEX
+        .captures(key)
+        .context(InvalidAuthorString)?;
+    let key_str = caps.get(1).context(InvalidAuthorString)?;
+    let mut buff = [0; 32];
+    decode_config_slice(key_str.as_bytes(), base64::STANDARD, &mut buff)
+        .context(InvalidAuthorStringBase64Encoding)?;
+    Ok(buff)
 }
 
 #[cfg(test)]
 mod tests {
 
-    use crate::{
-        extract_value_from_entry, get_key_bytes, get_sig_bytes, remove_signature_from_entry, verify,
-    };
+    use crate::{get_key_bytes, get_sig_bytes, verify};
     use base64;
 
     #[test]
     fn it_works() {
         let msg = VALID_MESSAGE.as_bytes();
-        assert!(verify(&msg));
+        assert!(verify(&msg).is_ok());
     }
 
     #[test]
     fn get_sig_bytes_works() {
         let expected = base64::decode("PkZ34BRVSmGG51vMXo4GvaoS/2NBc0lzdFoVv4wkI8E8zXv4QYyE5o2mPACKOcrhrLJpymLzqpoE70q78INuBg==").unwrap();
-        let bytes = get_sig_bytes(VALID_MESSAGE.as_bytes());
+        let bytes = get_sig_bytes("PkZ34BRVSmGG51vMXo4GvaoS/2NBc0lzdFoVv4wkI8E8zXv4QYyE5o2mPACKOcrhrLJpymLzqpoE70q78INuBg==.sig.ed25519".as_bytes()).unwrap();
         assert_eq!(&bytes[..], &expected[..]);
     }
 
     #[test]
     fn get_key_bytes_works() {
         let expected = base64::decode("U5GvOKP/YUza9k53DSXxT0mk3PIrnyAmessvNfZl5E0=").unwrap();
-        let bytes = get_key_bytes(VALID_MESSAGE.as_bytes());
+        let bytes =
+            get_key_bytes("@U5GvOKP/YUza9k53DSXxT0mk3PIrnyAmessvNfZl5E0=.ed25519".as_bytes())
+                .unwrap();
         assert_eq!(&bytes[..], &expected[..]);
     }
 
-    #[test]
-    fn remove_signature_from_entry_works() {
-        let msg = VALID_ENTRY.as_bytes();
-        let result = remove_signature_from_entry(&msg);
-        assert_eq!(result, VALID_ENTRY_NO_SIGNATURE.as_bytes());
-    }
-
-    #[test]
-    fn extract_value_from_entry_works() {
-        let msg = VALID_MESSAGE.as_bytes();
-        let result = extract_value_from_entry(&msg);
-
-        assert_eq!(&result, VALID_ENTRY);
-    }
     const VALID_MESSAGE: &str = r##"{
   "key": "%kmXb3MXtBJaNugcEL/Q7G40DgcAkMNTj3yhmxKHjfCM=.sha256",
   "value": {
@@ -154,34 +222,5 @@ mod tests {
     "signature": "PkZ34BRVSmGG51vMXo4GvaoS/2NBc0lzdFoVv4wkI8E8zXv4QYyE5o2mPACKOcrhrLJpymLzqpoE70q78INuBg==.sig.ed25519"
   },
   "timestamp": 1571140551543
-}"##;
-
-    const VALID_ENTRY: &str = r##"{
-  "previous": "%IIjwbJbV3WBE/SBLnXEv5XM3Pr+PnMkrAJ8F+7TsUVQ=.sha256",
-  "author": "@U5GvOKP/YUza9k53DSXxT0mk3PIrnyAmessvNfZl5E0=.ed25519",
-  "sequence": 8,
-  "timestamp": 1470187438539,
-  "hash": "sha256",
-  "content": {
-    "type": "contact",
-    "contact": "@ye+QM09iPcDJD6YvQYjoQc7sLF/IFhmNbEqgdzQo3lQ=.ed25519",
-    "following": true,
-    "blocking": false
-  },
-  "signature": "PkZ34BRVSmGG51vMXo4GvaoS/2NBc0lzdFoVv4wkI8E8zXv4QYyE5o2mPACKOcrhrLJpymLzqpoE70q78INuBg==.sig.ed25519"
-}"##;
-
-    const VALID_ENTRY_NO_SIGNATURE: &str = r##"{
-  "previous": "%IIjwbJbV3WBE/SBLnXEv5XM3Pr+PnMkrAJ8F+7TsUVQ=.sha256",
-  "author": "@U5GvOKP/YUza9k53DSXxT0mk3PIrnyAmessvNfZl5E0=.ed25519",
-  "sequence": 8,
-  "timestamp": 1470187438539,
-  "hash": "sha256",
-  "content": {
-    "type": "contact",
-    "contact": "@ye+QM09iPcDJD6YvQYjoQc7sLF/IFhmNbEqgdzQo3lQ=.ed25519",
-    "following": true,
-    "blocking": false
-  }
 }"##;
 }
